cd8a6816c9b955f7ba9b16abf03ff00951d4917b
chadbarrows <chad.m.barrows@gmail.com>
2018-11-26 21:05:45 -0500

    added map and sidebar section

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import scriptLoader from "react-async-script-loader";
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
+	state = {
+		map: {}
+	}
+	
+	componentWillReceiveProps() {
+		const map = new window.google.maps.Map(document.getElementById('map'), {
+			center: {lat: 39.616381, lng: -77.692471},
+			zoom: 14,
+			mapTypeControl: false
+		});
+		this.setState({map:map});
+	}
 
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<section id="list">
+					<h1>Hagerstown, MD</h1>
+					<hr />
+					<p>Test</p>
+				</section>
+				<div id='map'></div>
+			</div>
+		);
+	}
 }
 
-export default App;
+// export default App;
+export default scriptLoader([
+	`https://maps.googleapis.com/maps/api/js?key=AIzaSyA-5Vwx42DwuoHH7CqTVMFtaJYCAqD9J3s`
+ ])(App);
