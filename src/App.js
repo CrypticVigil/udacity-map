1e3171ba744bea56e4f260e06b44c9d3245c5fd0
chadbarrows <chad.m.barrows@gmail.com>
2018-11-27 21:36:47 -0500

    added markers and filter input

--- src/App.js
+++ src/App.js
@@ -1,28 +1,89 @@
 import React, { Component } from 'react';
 import scriptLoader from "react-async-script-loader";
+import { locationList } from './locations';
+import { mapStyles } from './mapStyles';
 import './App.css';
 
 class App extends Component {
 	state = {
-		map: {}
+		map: {},
+		locations: locationList,
+		markerList: [],
+		filtered: [],
+		openMarker: {
+			last: {},
+			closed: true
+		},
+		query: ''
 	}
 	
 	componentWillReceiveProps() {
+		const self = this;
 		const map = new window.google.maps.Map(document.getElementById('map'), {
-			center: {lat: 39.616381, lng: -77.692471},
+			center: {lat: 39.615019, lng: -77.702539},
 			zoom: 14,
-			mapTypeControl: false
+			mapTypeControl: false,
+			styles: mapStyles
 		});
+		for (let item of locationList) {
+			const position = {
+				lat: item.location.lat,
+				lng: item.location.lng
+			}
+			const marker = new window.google.maps.Marker({
+				position,
+				map,
+				title: item.title,
+				infoWindow: new window.google.maps.InfoWindow({
+					content: item.title
+				})
+			});
+			marker.addListener('click', function() {
+				if (self.state.openMarker.closed) {
+					marker.infoWindow.open(map, marker);
+					self.setState({ openMarker: { last: marker, closed: false} });
+				} else {
+					self.state.openMarker.last.infoWindow.close(map, marker);
+					self.setState({ openMarker: {closed: true} });
+				}
+			});
+			this.state.markerList.push(marker);
+		}
 		this.setState({map:map});
+		let { markerList } = this.state;
+		this.setState({filtered: markerList })
+	}
+
+	inputHandler = event => {
+		const { markerList } = this.state;
+		const query = event.target.value;
+		this.setState({ query });
+
+		const filterList = markerList.filter( item => {
+			return item.title.toLowerCase().indexOf(query) > -1;
+		});
+		this.setState({ filtered: filterList });
 	}
 
 	render() {
+		let { query, filtered } = this.state;
+
 		return (
 			<div className="App">
 				<section id="list">
 					<h1>Hagerstown, MD</h1>
-					<hr />
-					<p>Test</p>
+					<input
+						type="text"
+						placeholder="Filter locations"
+						value={query}
+						onChange={this.inputHandler}
+						className="filter"
+        			/>
+					<ul>
+						{ filtered.map( (item) => (
+							<li key={item.title} className='location'>{item.title}</li>
+						))}
+					</ul>
 				</section>
 				<div id='map'></div>
 			</div>
---
+++ src/locations.js
@@ -0,0 +1,81 @@
+export const locationList = [{
+	location: {
+	  lat: 39.613917,
+	  lng: -77.693224
+	},
+	title: "Martins' Food",
+	address: '1729 Dual Hwy, Hagerstown, MD 21740'
+  },
+  {
+	location: {
+	  lat: 39.616126,
+	  lng: -77.670884
+	},
+	title: 'Washington County Regional Park',
+	address: '20025 Mt Aetna Rd, Hagerstown, MD 21742'
+  },
+  {
+	location: {
+	  lat: 39.607887,
+	  lng: -77.681130
+	},
+	title: 'Londontowne Apartments',
+	address: '900 Queen Annes Ct, Hagerstown, MD 21740'
+  },
+  {
+	location: {
+	  lat: 39.622017,
+	  lng: -77.696621
+	},
+	title: 'Covenant Life Church',
+	address: '1515 Dual Hwy, Hagerstown, MD 21740'
+	},
+  {
+	location: {
+	  lat: 39.614883,
+	  lng: -77.691030
+	},
+	title: "Little Caesars Pizza",
+	address: '1722 Dual Hwy, Hagerstown, MD 21740'
+  },
+  {
+	location: {
+	  lat: 39.609609,
+	  lng: -77.707001
+	},
+	title: 'Animal Health Clinic of Funkstown',
+	address: '26 E Baltimore St, Funkstown, MD 21734'
+	},
+  {
+	location: {
+	  lat: 39.599649,
+	  lng: -77.733352
+	},
+	title: 'Walmart Supercenter',
+	address: '10420 Walmart Dr, Hagerstown, MD 21740'
+  },
+  {
+	location: {
+	  lat: 39.626465,
+	  lng: -77.729655
+	},
+	title: "Krumpe's Do-Nut Shop",
+	address: '912 Maryland Ave, Hagerstown, MD 21740'
+  },
+  {
+	location: {
+	  lat: 39.608367,
+	  lng: -77.733840
+	},
+	title: 'Hagerstown Premium Outlets',
+	address: '495 Premium Outlets Blvd, Hagerstown, MD 21740'
+  },
+  {
+	location: {
+	  lat: 39.626885,
+	  lng: -77.698119
+	},
+	title: "Turner's Bowling Alley",
+	address: '1350 Dual Hwy, Hagerstown, MD 21740'
+  }
+];
